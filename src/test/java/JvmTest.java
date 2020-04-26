4246182acd2f62ba19f27761f7411570d52c4652
SeanLiew <471293295@qq.com>
2020-04-26 19:29:26 +0800

    test

---
+++ src/test/java/JvmTest.java
@@ -0,0 +1,12 @@
+/**
+ * desc:
+ * author: liuxiaozheng
+ * time: 2020/4/23  18:54
+ **/
+public class JvmTest {
+
+
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
