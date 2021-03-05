eb6d6fc8c8221cb251fb3d9155a51f124803495d
SeanLiew <471293295@qq.com>
2021-03-05 10:07:24 +0800

    io

---
+++ src/test/java/iol/RequestHandler.java
@@ -0,0 +1,13 @@
+package iol;
+
+/**
+ * desc:
+ * author: liuxiaozheng
+ * time: 2021/2/25  16:43
+ **/
+public class RequestHandler {
+    public String handle(String request) {
+        System.out.println(request);
+        return "123";
+    }
+}
